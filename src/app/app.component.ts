27981f174bfc39574c5081110fedd4b53d473ca4
Ahmed Galadi <galadiahmed99@gmail.com>
2021-09-09 18:30:32 +0100

    add firebase and authentication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'share-your-opinion-app';
+
+  constructor() {
+    const firebaseConfig = {
+      apiKey: "AIzaSyBvFENvJhsI_EPLG2R4uTpCewrsP5YUsBI",
+      authDomain: "share-ur-op.firebaseapp.com",
+      projectId: "share-ur-op",
+      storageBucket: "share-ur-op.appspot.com",
+      messagingSenderId: "935345095721",
+      appId: "1:935345095721:web:e677e64d6cb2b308b70838",
+      measurementId: "G-5KF50P43FL"
+    };
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+  }
+
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { getAuth, createUserWithEmailAndPassword, Auth, signInWithEmailAndPassword } from 'firebase/auth';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,43 @@ import { Injectable } from '@angular/core';
 export class AuthService {
 
   constructor() { }
+
+//For Creating Users
+  creatUser(email: string, password: string) {
+    return new Promise<void>(
+      (resolve, reject) => {
+        const auth: Auth = getAuth();
+        createUserWithEmailAndPassword(auth, email, password).then(
+          () => {
+            resolve();
+          },
+          (error) => {
+            reject(error);
+          }
+        );
+      }
+    );
+  }
+
+//For User Sign-in
+  signInUser(email: string, password: string) {
+    return new Promise<void>(
+      (resolve, reject) => {
+        const auth: Auth = getAuth();
+        signInWithEmailAndPassword(auth, email, password).then(
+          () => {
+            resolve();
+          },
+          (error) => {
+            reject(error);
+          }
+        );
+      }
+    );
+  }
+
+  //For User Sign-out
+  signOut() {
+    getAuth().signOut();
+  }
 }
