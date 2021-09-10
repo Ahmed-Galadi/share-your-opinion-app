aaeaf5d0927a4e36ba3315abc037d873d38d93d9
Ahmed Galadi <galadiahmed99@gmail.com>
2021-09-10 03:07:34 +0100

    posts interface

--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -1,11 +1,14 @@
 import { Post } from './../models/Post.model';
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
 
+  posts: Post[] = [];
+  booksSubject = new Subject<Post[]>();
 
   constructor() { }
 }
--- src/app/services/upload.service.ts
+++ src/app/services/upload.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage'
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,29 @@ import { Injectable } from '@angular/core';
 export class UploadService {
 
   constructor() { }
+
+  uploadFile(file: File) {
+    return new Promise<string>(
+      (resolve, reject) => {
+        const almostUniqueFileName = Date.now().toString();
+        const storage = getStorage();
+        const storageRef = ref(storage, `images/${almostUniqueFileName}${file.name}`);
+        const upload = uploadBytesResumable(storageRef, file);
+
+        upload.on('state_changed',
+          () => {
+            console.log('Loading...');
+          },
+          (error) => {
+            console.log(`Loading Error: ${error}`);
+            reject();
+          },
+          () => {
+            resolve(getDownloadURL(upload.snapshot.ref));
+            console.log('Downloaded !!')
+          }
+        );
+      }
+    );
+  }
 }
