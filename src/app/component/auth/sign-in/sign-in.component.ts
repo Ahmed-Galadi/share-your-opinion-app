07a33bc223ef01c7173d0038ec1103b1b5969b3d
Ahmed Galadi <galadiahmed99@gmail.com>
2021-09-28 20:36:55 +0100

    make footer

--- src/app/component/auth/sign-in/sign-in.component.ts
+++ src/app/component/auth/sign-in/sign-in.component.ts
@@ -27,10 +27,6 @@ export class SignInComponent implements OnInit {
     this.signInForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.pattern(/[0-9a-zA-Z]{6,}/)]],
-      confirmPassword: ['',[Validators.required, Validators.pattern(/[0-9a-zA-Z]{6,}/)]]
-    },
-    {
-      validator: ConfirmedValidator('password', 'confirmPassword')
     });
   }
 
